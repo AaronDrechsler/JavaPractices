bf9c6a0b11394c522cc8068d4c7837b1770e31b2
Aaron Drechsler <a.drechsler@xceptance.net>
2020-12-21 14:21:58 +0100

    Remone duplicates from ArrayList

--- src/RemoveFromString.java
+++ src/RemoveFromString.java
@@ -1,19 +1,43 @@
-import java.util.Arrays;
 
+import java.util.*;
 public class RemoveFromString {
-	public static String removeFromStringArray(String[] s) {
-		
-
-		for (int i = 0; i < s.length; i++) 
+	
+	public static HashSet<String> sortArrayList(ArrayList<String> list) {
+		//loops thru the ArrayList
+		for (int i = 0; i < list.size(); i++) 
 		{
-		   for(int j = 0; j < s.length; j++) 
-		   {
-			   if()
-		   }
+			for (int j = i + 1; j < list.size(); j++) 
+			{
+				// checks the letter value
+				if (list.get(i).compareTo(list.get(j)) > 0) 
+				{
+					//switch`s the words
+					final String temp = list.get(i);
+					list.set(i, list.get(j));
+					list.set(j, temp);
+				}
+			}
 		}
-		return;
-}
-	public static void main(String args[]) {
-	System.out.print(removeFromStringArray(new String[] {}));
-	}
+		HashSet newList = new HashSet(list);
+		return newList;
+		}
+	
+		public static HashSet<String> findDuplicates(HashSet<String> list){
+			HashSet<String> items = new HashSet<String>(list); 
+			HashSet<String> duplicates = new HashSet<String>();           
+			for (String item : list) 
+			{   
+				if (items.contains(item)) 
+					{   
+					duplicates.add(item);              
+					} 
+				else 
+					{ 
+						items.add(item);               
+					}
+			}
+			ArrayList newDuplicate = new ArrayList(duplicates);
+			sortArrayList(newDuplicate);
+			return duplicates;       
+			}
 }
--- src/sortArrayListTest.java
+++ src/sortArrayListTest.java
@@ -8,14 +8,23 @@ class sortArrayListTest {
 	@Test
 	void testWithNormalCharacters() {
 		assertEquals(new ArrayList<String>(Arrays.asList("Aaron", "Anja", "Chris", "Jakob")), sortArrayList.sortArrayList(new ArrayList<String>(Arrays.asList("Chris", "Aaron", "Jakob", "Anja"))));
-		assertEquals(new ArrayList<String>(Arrays.asList("AARON", "ANJA", "CHRIS", "JAKOB")), sortArrayList.sortArrayList(new ArrayList<String>(Arrays.asList("CHRIS", "AARON", "JAKOB", "ANJA"))));
-		assertEquals(new ArrayList<String>(Arrays.asList("aaron", "anja", "chris", "jakob")), sortArrayList.sortArrayList(new ArrayList<String>(Arrays.asList("chris", "aaron", "jakob", "anja"))));
+		assertEquals(new ArrayList<String>(Arrays.asList("AARON", "ANJA", "APPLE", "CHRIS", "JAKOB", "SERVER")), sortArrayList.sortArrayList(new ArrayList<String>(Arrays.asList("CHRIS", "AARON", "JAKOB", "ANJA", "APPLE", "SERVER"))));
+		assertEquals(new ArrayList<String>(Arrays.asList("aaron", "anja", "apple", "chris", "jakob", "server")), sortArrayList.sortArrayList(new ArrayList<String>(Arrays.asList("chris", "aaron", "jakob", "anja", "apple", "server"))));
 		assertEquals(new ArrayList<String>(Arrays.asList("AAA", "BBB", "aaa", "bbb")), sortArrayList.sortArrayList(new ArrayList<String>(Arrays.asList("AAA", "aaa", "BBB", "bbb"))));
 		assertEquals(new ArrayList<String>(Arrays.asList("Aaron", "Bbibel", "aaron", "bbibel")), sortArrayList.sortArrayList(new ArrayList<String>(Arrays.asList("bbibel", "Bbibel", "Aaron", "aaron"))));
-		
+		assertEquals(new ArrayList<String>(Arrays.asList("Aaron", "Bbibel", "aaron", "bbibel")), sortArrayList.sortArrayList(new ArrayList<String>(Arrays.asList("bbibel", "Bbibel", "Aaron", "aaron"))));
+
 	}
+	@Test
 	void testWithNumbers() {
+		assertEquals(new ArrayList<String>(Arrays.asList("FOS20T1", "FOS20T2", "FOS20T3", "FOS20T4")), sortArrayList.sortArrayList(new ArrayList<String>(Arrays.asList("FOS20T4", "FOS20T2", "FOS20T1", "FOS20T3"))));
 		assertEquals(new ArrayList<String>(Arrays.asList("Aaron1", "Anja2", "Chris1", "Jakob2")), sortArrayList.sortArrayList(new ArrayList<String>(Arrays.asList("Chris1", "Aaron1", "Jakob2", "Anja2"))));
+		assertEquals(new ArrayList<String>(Arrays.asList("AudiA3", "AudiA4", "AudiA6", "AudiA9")), sortArrayList.sortArrayList(new ArrayList<String>(Arrays.asList("AudiA6", "AudiA3", "AudiA4", "AudiA9"))));	
+		assertEquals(new ArrayList<String>(Arrays.asList("AudiA13", "AudiA14", "AudiA16", "AudiA19")), sortArrayList.sortArrayList(new ArrayList<String>(Arrays.asList("AudiA16", "AudiA13", "AudiA14", "AudiA19"))));	
+	}
+	@Test
+	void testWithJustNumbers() {
+		assertEquals(new ArrayList<String>(Arrays.asList("11", "21", "31", "41")), sortArrayList.sortArrayList(new ArrayList<String>(Arrays.asList("41", "21", "11", "31"))));
 		assertEquals(new ArrayList<String>(Arrays.asList("1", "2", "3", "4")), sortArrayList.sortArrayList(new ArrayList<String>(Arrays.asList("1", "4", "3", "2"))));	
 		assertEquals(new ArrayList<String>(Arrays.asList("Aaron1", "Aaron2", "Aaron3", "Aaron4")), sortArrayList.sortArrayList(new ArrayList<String>(Arrays.asList("Aaron4", "Aaron2", "Aaron1", "Aaron3"))));
 	}
