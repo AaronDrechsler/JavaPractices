0cb39986a635a4d84bc7e99f6b714ce8a8146719
Rene Schwietzke <r.schwietzke@xceptance.net>
2021-02-11 17:06:27 +0100

    Just clean and nice imports

--- src/MergingArraysTest.java
+++ src/MergingArraysTest.java
@@ -1,22 +1,14 @@
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
-
-import static org.junit.Assert.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-	import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-	import static org.junit.jupiter.api.Assertions.assertNull;
-
-	import java.util.ArrayList;
-	import java.util.Arrays;
-	import java.util.Collections;
-	import java.util.List;
-	import java.util.stream.Stream;
-
-	import org.junit.jupiter.api.Test;
-	import org.junit.jupiter.params.ParameterizedTest;
-	import org.junit.jupiter.params.provider.Arguments;
-	import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 
 /**
